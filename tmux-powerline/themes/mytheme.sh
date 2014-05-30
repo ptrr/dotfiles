e0c89b2cf796960fa0f36284bd8ea48bab6d5a5a
Peter de Ruijter <peter@l2code.nl>
2014-05-30 13:51:46 +0200

    Derp

---
+++ tmux-powerline/themes/mytheme.sh
@@ -0,0 +1,59 @@
+# Default Theme
+
+# if patched_font_in_use; then
+# 	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="⮂"
+# 	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="⮃"
+# 	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="⮀"
+# 	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="⮁"
+# else
+	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="||"
+	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="|"
+	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="||"
+	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="|"
+#fi
+
+TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'235'}
+TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'255'}
+
+TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}
+TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}
+
+
+# Format: segment_name background_color foreground_color [non_default_separator]
+
+if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
+	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
+		# "tmux_session_info 148 234" \
+		"hostname 232 251" \
+		"weather 232 14" \
+		"battery 232 127" \
+		# "vcs_branch 232 226" \
+		# "vcs_dirty 232 220" \
+		# "vcs_compare 232 220" \
+		# "vcs_staged 232 220" \
+		# "vcs_modified 232 220" \
+		# "vcs_others 232 0" \
+	)
+fi
+
+if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
+	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+		#"earthquake 3 0" \
+		# "pwd 232 251" \
+		# "mailcount 9 255" \
+		# "now_playing 234 37" \
+		# "cpu 240 136" \
+		# "load 237 167" \
+		# "ifstat_sys 232 34" \
+		"ifstat 232 34" \
+		# "lan_ip 232 87" \
+		# "wan_ip 232 87" \
+		"tmux_mem_cpu_load 232 226" \
+		"time 232 136" \		
+		# "rainbarf 232 0" \
+		#"xkb_layout 125 117" \
+		# "date_day 235 136" \
+		# "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		
+	)
+fi
